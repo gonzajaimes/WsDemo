47abc233ddcbd738467780e483cfe4de49df48f8
gonzajaimes <gonzajaimes@hotmail.com>
2018-09-13 08:35:11 -0500

    Create MVVM Solution with List

---
+++ DemoApp/DemoApp/Services/EmployeesServices.cs
@@ -0,0 +1,45 @@
+﻿using DemoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoApp.Services
+{
+    public class EmployeesServices
+    {
+        public List<Employee> GetEmployee()
+        {
+            var list = new List<Employee>
+            {
+                new Employee
+                {
+                    Name = "Mohamed",
+                    Department = "Marketing"
+                },
+
+                new Employee
+                {
+                    Name = "Pedro",
+                    Department = "Sales"
+                },
+
+                new Employee
+                {
+                    Name = "Pablo",
+                    Department = "Marketing"
+                },
+
+                new Employee
+                {
+                    Name = "Luis",
+                    Department = "Operations"
+                },
+
+
+            };
+
+            return list;
+        }
+
+    }
+}
---
+++ DemoApp/DemoApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using DemoApp.Models;
+using DemoApp.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DemoApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private List<Employee> _employeesList;
+
+        public List<Employee> EmployeesList
+        {
+            get { return _employeesList; }
+            set
+            {
+                _employeesList = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            var employeesServices = new EmployeesServices();
+            EmployeesList = employeesServices.GetEmployee();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
